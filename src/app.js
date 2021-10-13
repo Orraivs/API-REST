d0a7a1e51a5d610e0491a566742f115e7c745408
David Celis <davidcelis682@gmail.com>
2021-10-13 11:55:49 -0500

    Ypdate

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const app = express();
 const user = require('./routes/user');
 const play = require('./routes/play');
 
+localhost:3000/nombre/sneyder
+
+app.get('/:nombre/:apellido',(req,res)=>{
+    let {nombre, apellido} = req.params;
+    res.status(404).send('Hola '+nombre+' '+ap)
+})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 

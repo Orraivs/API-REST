1a943f70bd41841dad7f0a3a108b8b6e8b4eac20
David Celis <davidcelis682@gmail.com>
2021-10-18 14:45:15 -0500

    Update

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 exports.find = (req, res, next) => {
     let params = {};
     params["user"] = req.body.user;
@@ -14,18 +16,15 @@ exports.find = (req, res, next) => {
 }
 
 exports.signin = async (req, res) => {
-    /*
-    req.user
-    req.password
-    */
     if (req.body.e) return res.status(500).send({ e });
     if (!req.body.users) return res.status(404).send({ message: 'NOT FOUND!' });
-    let users = req.body.users[0];
-    const match = await bcrypt.compare(req.body.password, users.password);
-    res.send({
-        message: 'La contraseña es: ' + users.password,
-        match
-    })
-
-
+    let user = req.body.users[0];
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if(match){
+        jwt.sign({ user }, 'privateKey', function(err, token) {
+            res.send({token})
+        });
+    }else{
+        res.send({message: 'Invalid credentials!'})
+    }  
 }
